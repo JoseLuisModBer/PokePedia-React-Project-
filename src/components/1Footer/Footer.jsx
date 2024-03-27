83869cbb4574322d86caf6201dc1f06d5ca0d81e
Jose Luis <joseluismodronobw@gmail.com>
2024-03-27 11:49:20 +0100

    Correcciones en footer y header

--- src/components/1Footer/Footer.jsx
+++ src/components/1Footer/Footer.jsx
@@ -3,9 +3,19 @@ import './Footer.css';
 /*------------------------COMPONENTE FOOTER----------------------------*/
 
 export const Footer = () => {
+  // Obtenemos el año actual (para la función de mostrarTodosCumpleanos)
+  const hoy = new Date();
+  const añoActual = hoy.getFullYear();
+
   return (
     <footer>
-      <p>&copy; Jose Luis Modroño Berdiñas 2024</p>
+      <p>
+        &copy;{' '}
+        <a href="https://www.linkedin.com/in/joseluismodro%C3%B1oberdi%C3%B1as/">
+          Jose Luis Modroño Berdiñas
+        </a>
+        <span> {añoActual}</span>
+      </p>
     </footer>
   );
 };
